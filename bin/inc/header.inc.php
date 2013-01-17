0439d6e1bef8eb99c4f92ec78cc2bd5007b56724
davetaz <davetaz@ref.(none)>
2013-01-17 17:38:48 +0000

    Added library tools and removed tempory files that are generated by the scripts

---
+++ bin/inc/header.inc.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>
+<?php echo $page_title; ?>
+</title>
+	<link rel="stylesheet" type="text/css" href="/css/style.css" />
+</head>
+<body>
+<h1>
+<?php 
+if ($main_title != "") {
+	echo $main_title;
+} else {
+	echo 'Preserv2 - File Format Registry (beta)';
+}
+?>
+</h1>
+<hr class="full_rule"/>
+<div class="body_block">
